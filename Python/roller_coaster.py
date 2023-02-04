9c221f869458291e6cc25ab73eb7e59e1dca0c8a
nevagee <monika.grabovska@gmail.com>
2023-02-04 23:25:33 +0100

    dalsi ukol

---
+++ Python/roller_coaster.py
@@ -0,0 +1,10 @@
+#conditions
+
+print("Vitejte na horske draze")
+height = int(input("Jaka je vase vyska v cm?\n"))
+
+if height >= 87:
+    print("Muzete jet na horske draze")
+
+else: #DODRZUJ ODSAZENI
+    print("Omlouvame se, ale na horske draze jet nemuzete.")
