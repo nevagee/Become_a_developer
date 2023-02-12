291fa7f9fe8d257fd622ff74ac7914de8fbb803d
nevagee <monika.grabovska@gmail.com>
2023-02-12 21:51:25 +0100

    new

---
+++ Python/3rd lesson/Print_length_of_string.py
@@ -0,0 +1,14 @@
+#Print the Length of a String
+#print(len(s))
+
+a = ""
+b = "H"
+c = "Hello"
+d = "Amazing"
+s = "python"
+
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+print(len(s))
