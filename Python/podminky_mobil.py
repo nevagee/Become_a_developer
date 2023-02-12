133a74766a0a26c5e8ae94ccb655839eeae5ab98
nevagee <monika.grabovska@gmail.com>
2023-02-12 16:35:17 +0100

    conditions_exercise

---
+++ Python/podminky_mobil.py
@@ -0,0 +1,9 @@
+# Podminky - normal, smart, extrasmart
+
+type = input("Jaky chcete typ mobilniho telefonu? Moznosti: normal, smart, extrasmart\n")
+
+if type != "normal":
+    print(f"Cena tefelonu typu {type} je 25 000 KC")
+else: 
+    print(f"Cena telefonu typu {type} je 15 000 KC")
+
